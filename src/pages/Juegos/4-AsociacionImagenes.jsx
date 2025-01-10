5b4905a8000500b3d0a7d4300afa5b800430a06b
JulioCRV <julioroquevalda6@gmail.com>
2025-01-10 13:08:04 -0400

    Diseño juego 4

--- src/pages/Juegos/4-AsociacionImagenes.jsx
+++ src/pages/Juegos/4-AsociacionImagenes.jsx
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./4-AsociacionImagenes.css";
+import ModalGanar from "../../components/ModalCorrecto";
+import ModalPerder from "../../components/ModalIncorrecto";
 
 const ImageAssociationGame = () => {
   const [failedAttempts, setFailedAttempts] = useState(0);
   const [time, setTime] = useState(0);
   const [index, setIndex] = useState(Math.floor(Math.random() * 5) + 1);
+  const [openModal, setOpenModal] = useState(false);
+  const [isCorrect, setIsCorrect] = useState(false);
 
   const texts = [
     "Juego balon con una pelota, navego en un",
@@ -24,7 +28,7 @@ const ImageAssociationGame = () => {
 
   const images = numeros.map((num, i) => ({
     id: i + 1,
-    src: `/src/assets/g4${num}.svg`,
+    src: `/src/assets/Juego4/g4${num}.svg`,
     isCorrect: num === index,
   }));
 
@@ -40,31 +44,39 @@ const ImageAssociationGame = () => {
 
   const handleImageClick = (image) => {
     if (image.isCorrect) {
-      alert("¡Correcto! Has seleccionado la imagen correcta.");
+      // alert("¡Correcto! Has seleccionado la imagen correcta.");
       //Implementar logica para cargar la informacion y al usuario
       setIndex(Math.floor(Math.random() * 5) + 1); // Nuevo índice aleatorio
       setFailedAttempts(0);
       setTime(0);
+      setOpenModal(true);
+      setIsCorrect(true);
     } else {
       const newAttempts = failedAttempts + 1;
       setFailedAttempts(newAttempts);
       if (newAttempts >= 3) {
-        alert("¡Perdiste! Reinicia el juego.");
+        // alert("¡Perdiste! Reinicia el juego.");
         setFailedAttempts(0);
         setIndex(Math.floor(Math.random() * 5) + 1);
         setTime(0);
+        setOpenModal(true);
+        setIsCorrect(false);
       }
     }
   };
 
+  const closeModal = () =>{
+    setOpenModal(false);
+  }
+
   return (
     <div className="game-container">
       <div className="failed-attempts">
-        <strong>Intentos fallidos:</strong> {failedAttempts}/3
+        <p className="again">Intentos fallidos: {failedAttempts}/3</p>
+        <div className="timer">⏱️ {time}s</div>
       </div>
       <div className="title-container">
-        <h2>Asociación con imágenes</h2>
-        <div className="timer">⏱️ {time}s</div>
+        <h2 className="observa-title">Asociación con imágenes</h2>
       </div>
       <div className="generated-text">{generatedText}</div>
       <div className="image-grid">
@@ -78,6 +90,13 @@ const ImageAssociationGame = () => {
           />
         ))}
       </div>
+      {openModal && (<>
+        {isCorrect === true ? (
+          <ModalGanar text={"¡CORECCTO! Has seleccionado la imagen correcta."} activarBoton={"si"} resetGame={closeModal}/>) :
+          (<ModalPerder text={"¡PERDISTE! Reinicia el juego."} activarBoton={"si"} resetGame={closeModal} />)}
+      </>)
+
+      }
     </div>
   );
 };
