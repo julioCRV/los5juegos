22e2df83255e6e84ea4fca26dd8aabd72ae0848c
JulioCRV <julioroquevalda6@gmail.com>
2025-01-30 12:09:29 -0400

    Modal de inicio de sesión con conexión a la base de datos

--- src/pages/IniciarSesion.jsx
+++ src/pages/IniciarSesion.jsx
@@ -1,19 +1,55 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import logoInicio from '/assets/iconos/logoInicio.svg'
-import './IniciarSesion.css'; // Importa el archivo CSS
+import logoInicio from '/assets/iconos/logoInicio.svg';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import './IniciarSesion.css';
+import Modal from '../components/ModalCredenciales';
 
 
 const LoginForm = () => {
     const navigate = useNavigate();
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [openModal, setOpenModal] = useState(false);
+    const [message, setMessage] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        // console.log('Nombre de Usuario:', username);g
-        // console.log('Contraseña:', password);
-        navigate('/Inicio')
+
+        const data = {
+            user_name: username,
+            password: password,
+        };
+
+        try {
+            const response = await fetch("https://afhasiajuegos.tech/login.php", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            });
+
+            const result = await response.json();
+
+            if (result.message === "Login exitoso") {
+                setOpenModal(true);
+                setMessage(true)
+                setTimeout(() => {
+                    navigate('/Inicio');
+                  }, 2400); 
+            } else {
+                setOpenModal(true);
+                setMessage(false);
+                setTimeout(() => {
+                    setOpenModal(false);
+                  }, 1600); 
+            }
+        } catch (error) {
+            console.error("Error en la solicitud:", error);
+            setMessage("Hubo un problema al conectar con el servidor.");
+        }
     };
 
     return (
@@ -40,19 +76,38 @@ const LoginForm = () => {
                         </div>
                         <div className="form-group">
                             <label htmlFor="password" className="label">Contraseña</label>
-                            <input
-                                type="password"
-                                id="password"
-                                className="input"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                placeholder="Contraseña"
-                                required
-                            />
+                            <div className="password-container">
+                                <input
+                                    type={showPassword ? 'text' : 'password'}  // Cambiar tipo de input según el estado
+                                    id="password"
+                                    className="input"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    placeholder="Contraseña"
+                                    required
+                                />
+                                <button
+                                    type="button"
+                                    className="eye-icon"
+                                    onClick={() => setShowPassword(!showPassword)}  // Cambiar el estado para mostrar/ocultar
+                                >
+                                    {!showPassword ? <FaEyeSlash /> : <FaEye />}
+                                </button>
+                            </div>
                         </div>
                         <button type="submit" className="button">INICIAR SESIÓN</button>
                     </form>
-
+                </div>
+                <div>
+                    {openModal && (
+                        <>
+                            {message === true ? (
+                                <Modal text={`Has iniciado sesión correctamente. ¡Bienvenido/a ${username}!`} type={'success'} />
+                            ) : (
+                                <Modal text={"Credenciales incorrectas. Intenta nuevamente."} />
+                            )}
+                        </>
+                    )}
                 </div>
             </div>
         </div>
